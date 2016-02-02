aab6b8af7743dc6a1eaffbebb26a7078266409f0
Benjamin Eckardt <Benjamin.Eckardt@gmx.net>
2016-02-02 20:23:19 +0100

    initial commit

---
+++ dom-processor.js
@@ -0,0 +1,18 @@
+'use strict';
+var cheerio = require('cheerio');
+var fs = require('fs');
+var configFactory;
+module.exports = {
+  config: function(factory) {
+    configFactory = factory;
+  },
+  process: function(html, currentPath) {
+    var $ = cheerio.load(html);
+    var configs = configFactory(currentPath);
+    configs.forEach(function(conf) {
+      var $el = $(conf.selector);
+      $el.replaceWith(conf.replace($el, $));
+    });
+    return $.html();
+  }
+};
